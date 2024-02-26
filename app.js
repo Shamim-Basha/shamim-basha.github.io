9ad416a1cba393f9638a69e45cb213e595912f27
Shamim Basha <120175806+Shamim-Basha@users.noreply.github.com>
2024-02-26 21:48:33 +0530

    removed lenis

--- app.js
+++ app.js
@@ -320,15 +320,3 @@ gsap.from(".out",{
     delay:3,
 })
 
-//smooth scroll
-const lenis = new Lenis()
-
-lenis.on('scroll', (e) => {
-//   console.log(e)
-})
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
